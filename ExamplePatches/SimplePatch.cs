c992546aa6185ad96b959c8bf994dd683e2bab3e
Bob S <shibdib@users.noreply.github.com>
2026-04-12 12:09:06 -0400

    Initial commit

---
+++ ExamplePatches/SimplePatch.cs
@@ -0,0 +1,53 @@
+﻿using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace SPTClientModExamples.ExamplePatches
+{
+    internal class SimplePatch : ModulePatch // all patches must inherit ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            // one way methods can be patched is by targeting both their class name and the name of the method itself
+            // the example in this patch is the Jump() method in the Player class
+            return AccessTools.Method(typeof(Player), nameof(Player.Jump));
+        }
+
+        public static int MaxJumps = 3;
+        public static int CompletedJumps = 0;
+
+        [PatchPrefix]
+        static bool Prefix()
+        {
+            // code in Prefix() method will run BEFORE original code is executed.
+            // if 'true' is returned, the original code will still run.
+            // if 'false' is returned, the original code will be skipped.
+
+            if (CompletedJumps < MaxJumps)
+            {
+                CompletedJumps++;
+                int remainingJumps = MaxJumps - CompletedJumps;
+
+                // we are using that LogSource variable we set up in the Plugin.cs file.
+                Plugin.LogSource.LogWarning($"You jumped! You have {remainingJumps} left!");
+
+                return true; // return true to run the original code and jump!
+            }
+            else
+            {
+                Plugin.LogSource.LogError("You have no jumps left!");
+
+                return false; // we are out of jumps, so we return false, preventing the original jump code from running.
+            }
+        }
+
+        [PatchPostfix]
+        static void Postfix()
+        {
+            // code in Postfix() method runs AFTER the original code is executed
+        }
+
+        // uncomment the 'new SimplePatch().Enable();' line in your Plugin.cs script to enable this patch.
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace SPTClientModExamples
+{
+    // first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this project.
+    [BepInPlugin("SPTClientModExamples.UniqueGUID", "SPTClientModExamples", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+
+        // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake()
+        {
+            // save the Logger to public static field so we can use it elsewhere in the project
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
+
+            // uncomment line(s) below to enable desired example patch, then press F6 to build the project
+            // if this solution is properly placed in a YourSPTInstall/Development folder, the compiled plugin will automatically be copied into YourSPTInstall/BepInEx/plugins
+            // new SimplePatch().Enable();
+        }
+    }
+}
